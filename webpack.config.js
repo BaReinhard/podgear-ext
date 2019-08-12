790a942de16f0cb7438b12d07ea27a0f6936d627
brett.reinhard <brett.reinhard@ticketmaster.com>
2019-08-12 12:45:24 -0700

    Updated fonts to load correctly

--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,11 @@ module.exports = (_env,argv)=> {
           options:{
             name:"img/[name].[ext]"
           }
-        }
+        },
+        {
+          test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+          loader : 'file-loader'
+      }
       ]
     },
     resolve: { extensions: ['*', '.js', '.jsx'] },
